9f4c2d7e335639d94fcbead931f59004652f28ea
AnggaPuspa <dexangga6>
2022-06-17 00:28:05 +0800

    first commit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+export const products = [
+  {
+    id: 1,
+    img: "https://anggapuspa.github.io/foto/pic/1.png",
+    link: "http://lama.dev",
+  },
+  {
+    id: 2,
+    img: "https://anggapuspa.github.io/foto/pic/2.jpeg",
+    link: "http://lama.dev",
+  },
+  {
+    id: 3,
+    img: "https://anggapuspa.github.io/foto/pic/3.png",
+    link: "http://lama.dev",
+  },
+  {
+    id: 4,
+    img: "https://anggapuspa.github.io/foto/pic/4.png",
+    link: "http://lama.dev",
+  },
+  {
+    id: 5,
+    img: "https://anggapuspa.github.io/foto/pic/7.png",
+    link: "http://lama.dev",
+  },
+  {
+    id: 6,
+    img: "https://anggapuspa.github.io/foto/pic/6.png",
+    link: "http://lama.dev",
+  },
+];
